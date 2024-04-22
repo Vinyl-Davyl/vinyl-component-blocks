df48915d2abee5875bf8af2c4cc2b9487a359601
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-04-22 03:45:09 +0100

    feat: completed button component

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Meta, StoryFn } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 import { Button } from ".";
 
 // Define the metadata for the Button component
@@ -9,19 +9,40 @@ const meta: Meta = {
   parameters: {
     layout: "centered",
   },
-  argTypes: {
-    // Define any argTypes if needed
-  },
+  //   argTypes: {
+  //     // Define any argTypes if needed
+  //   },
+  tags: ["autodocs"],
 };
 
 // Export the metadata as default
 export default meta;
 
 // Define the story for the Default button
-const Template: StoryFn = (args: any) => <Button {...args} />;
+// const Template: StoryFn = (args: any) => <Button {...args} />;
+
+type Story = StoryObj<typeof meta>;
 
 // Export the Default button story
-export const Default = Template.bind({});
-Default.args = {
-  children: "Click Me",
+export const Default: Story = {
+  args: {
+    children: "Click Me",
+    variant: "solid",
+    size: "md",
+    colorscheme: "primary",
+  },
+};
+
+export const Outline: Story = {
+  args: {
+    children: "Button",
+    variant: "outline",
+  },
+};
+
+export const Ghost: Story = {
+  args: {
+    children: "Button",
+    variant: "ghost",
+  },
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+import colors from "tailwindcss/colors";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.blue,
+      },
+    },
   },
   plugins: [],
 };
