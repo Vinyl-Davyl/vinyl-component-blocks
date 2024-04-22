80539a30faa247b038ea36f3638f773c1f505f57
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-04-22 04:56:22 +0100

    update: development

--- src/components/Text/index.stories.tsx
+++ src/components/Text/index.stories.tsx
@@ -16,6 +16,7 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
+    as: "h1",
     children:
       "Full test functionality of the vinyl-component-block library to be deployed as a package on NPM store, this is the text component test",
   },
--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,5 +1,11 @@
-import React, { ComponentProps, forwardRef } from "react";
+import React from "react";
+import { cn } from "@/utils";
+import {
+  PolymorphicComponentPropsWithRef,
+  PolymorphicRef,
+} from "@/utils/types";
 import { VariantProps, cva } from "class-variance-authority";
+import { forwardRef } from "react";
 
 const textStyles = cva("w-full", {
   variants: {
@@ -40,10 +46,49 @@ const textStyles = cva("w-full", {
   },
 });
 
-type TextProps = ComponentProps<"span"> & VariantProps<typeof textStyles>;
+type TextProps<C extends React.ElementType> = PolymorphicComponentPropsWithRef<
+  C,
+  VariantProps<typeof textStyles>
+>;
 
-export const Text = forwardRef<HTMLSpanElement, TextProps>(
-  ({ ...props }, ref) => {
-    return <span ref={ref} {...props} />;
+type TextComponent = <C extends React.ElementType = "span">(
+  props: TextProps<C>
+) => React.ReactElement | null;
+
+// @ts-expect-error - unexpected typing errors
+export const Text: TextComponent = forwardRef(
+  <C extends React.ElementType = "span">(
+    {
+      as,
+      align,
+      size,
+      emphasis,
+      italic,
+      underline,
+      weight,
+      className,
+      ...props
+    }: TextProps<C>,
+    ref?: PolymorphicRef<C>
+  ) => {
+    const Component = as || "span";
+
+    return (
+      <Component
+        ref={ref}
+        className={cn(
+          textStyles({
+            size,
+            weight,
+            emphasis,
+            italic,
+            underline,
+            align,
+            className,
+          })
+        )}
+        {...props}
+      />
+    );
   }
 );
