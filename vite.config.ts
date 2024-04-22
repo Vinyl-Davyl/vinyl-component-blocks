7cb8d6f874fb241f728745ab97d4bfefce57794e
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-04-22 15:11:23 +0100

    update: development

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "./src/components/index.tsx"),
+      name: "vinyl-blocks",
+      fileName: "vinyl-blocks",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+  plugins: [react(), dts({ rollupTypes: true })],
+});
