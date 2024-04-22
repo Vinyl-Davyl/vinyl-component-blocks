4a356860e97555245938d9e4e56fe2c1985d5296
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-04-22 04:34:04 +0100

    feat: text component

---
+++ src/components/Text/index.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Text } from ".";
+
+const meta: Meta<typeof Text> = {
+  title: "Components/Text",
+  component: Text,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children:
+      "Full test functionality of the vinyl-component-block library to be deployed as a package on NPM store, this is the text component test",
+  },
+};
---
+++ src/components/Text/index.tsx
@@ -0,0 +1,49 @@
+import React, { ComponentProps, forwardRef } from "react";
+import { VariantProps, cva } from "class-variance-authority";
+
+const textStyles = cva("w-full", {
+  variants: {
+    emphasis: {
+      low: "text-gray-600 font-light",
+    },
+    size: {
+      sm: "text-sm",
+      base: "text-base",
+      lg: "text-lg",
+      xl: "text-xl",
+      "2xl": "text-2xl",
+      "3xl": "text-3xl",
+    },
+    weight: {
+      thin: "font-thin",
+      normal: "font-normal",
+      medium: "font-medium",
+      semibold: "font-semibold",
+      bold: "font-bold",
+      black: "font-black",
+    },
+    align: {
+      left: "text-left",
+      center: "text-center",
+      right: "text-right",
+    },
+    italic: {
+      true: "italic",
+    },
+    underline: {
+      true: "underline underline-offset-2",
+    },
+  },
+  defaultVariants: {
+    size: "base",
+    align: "left",
+  },
+});
+
+type TextProps = ComponentProps<"span"> & VariantProps<typeof textStyles>;
+
+export const Text = forwardRef<HTMLSpanElement, TextProps>(
+  ({ ...props }, ref) => {
+    return <span ref={ref} {...props} />;
+  }
+);
