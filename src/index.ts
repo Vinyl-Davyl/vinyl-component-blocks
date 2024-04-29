b7581e4cb8d0f959c5a4b06a8a21712f3ad18586
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-04-29 21:23:24 +0100

    update: development

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-export * from "./components";
+import { Button } from "./components/Button/index";
+import { Input } from "./components/Input/index";
+// import { Layout } from "./components/Layout/index";
+import { Typography } from "./components/Typography/index";
+import { Avatar } from "./components/Avatar/index";
+import { Card } from "./components/Card/index";
+import { Checkbox } from "./components/Checkbox/index";
+import { Modal } from "./components/Modal/index";
+import { OverviewCard } from "./components/OverviewCard/index";
+import { Spinner } from "./components/Spinner/index";
+import { Tabs } from "./components/Tabs/index";
+
+export { Button };
+export { Input };
+// export {Layout}
+export { Typography };
+export { Avatar };
+export { Card };
+export { Checkbox };
+export { Modal };
+export { OverviewCard };
+export { Spinner };
+export { Tabs };
