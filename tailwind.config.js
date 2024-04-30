5ec9c98845e34b7a97c563759484197893f30cf9
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-04-30 19:57:34 +0100

    update: development

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ import colors from "tailwindcss/colors";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], './vinyl-blocks/vinyl-blocks/**/*.{html,ts,css,scss}'
   theme: {
     extend: {
       colors: {
